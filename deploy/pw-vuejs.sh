409ec97125c3ee5343cea27d2e271597a07dbd04
Ethan Steiner <esteiner@nuxeo.com>
2019-02-05 17:18:34 -0500

    logo change

---
+++ deploy/pw-vuejs.sh
@@ -0,0 +1,13 @@
+# navigate into project directory
+cd personal-website/
+
+# build our frontend project
+npm install
+npm run build
+
+# navigate into the directory which contains
+# our built frontend
+cd dist/
+
+# Push to S3
+# We've still to do this
